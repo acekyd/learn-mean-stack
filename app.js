728ec9311eb1072ad91c4fc4dc8495b7ba9cfd19
Abati Adewale <acekyd01@gmail.com>
2016-12-03 23:49:03 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+	'$scope',
+	function($scope){
+		$scope.test = 'Hello World!';
+		$scope.posts = [
+			{title: 'post 1', upvotes: 5},
+			{title: 'post 2', upvotes:2},
+			{title: 'post 3', upvotes:15},
+			{title: 'post 4', upvotes:9},
+			{title: 'post 5', upvotes:4}
+		];
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === ''){return;}
+			$scope.posts.push({title: $scope.title, upvotes: 0});
+			$scope.title = "";
+		};
+		$scope.incrementUpvotes = function(post){
+			post.upvotes += 1;
+		}
+	}]);
