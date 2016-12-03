24b0151960748d71d805c3577271349cb367a27a
Abati Adewale <acekyd01@gmail.com>
2016-12-04 00:20:16 +0100

    added bootstrap

--- app.js
+++ app.js
@@ -13,10 +13,15 @@ app.controller('MainCtrl', [
 		];
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ''){return;}
-			$scope.posts.push({title: $scope.title, upvotes: 0});
+			$scope.posts.push({
+				title: $scope.title, 
+				upvotes: 0,
+				link: $scope.link
+			});
 			$scope.title = "";
+			$scope.link = "";
 		};
 		$scope.incrementUpvotes = function(post){
 			post.upvotes += 1;
-		}
+		};
 	}]);
