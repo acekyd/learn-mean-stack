61015a7e69b5037012d1a66b365a31f73905dfca
Abati Adewale <acekyd01@gmail.com>
2016-12-10 01:05:28 +0100

    page templates

--- app.js
+++ app.js
@@ -1,27 +1,73 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
-app.controller('MainCtrl', [
-	'$scope',
-	function($scope){
-		$scope.test = 'Hello World!';
-		$scope.posts = [
-			{title: 'post 1', upvotes: 5},
+app.factory('posts', [function() {
+	//Service body
+	var o =	{
+		posts: [
+			{title: 'post 1', upvotes: 5, link: 'www.google.com'},
 			{title: 'post 2', upvotes:2},
 			{title: 'post 3', upvotes:15},
 			{title: 'post 4', upvotes:9},
 			{title: 'post 5', upvotes:4}
-		];
+		]
+	}
+	return o;
+}]);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			})
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			})
+
+		$urlRouterProvider.otherwise('home');
+	}]);
+
+app.controller('MainCtrl', [
+	'$scope', 'posts',
+	function($scope, posts){
+		$scope.test = 'Hello World!';
+		$scope.posts = posts.posts;
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ''){return;}
 			$scope.posts.push({
 				title: $scope.title, 
 				upvotes: 0,
-				link: $scope.link
+				link: $scope.link,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
 			});
 			$scope.title = "";
 			$scope.link = "";
 		};
 		$scope.incrementUpvotes = function(post){
 			post.upvotes += 1;
+		};
+	}]);
+app.controller('PostsCtrl', [
+	'$scope', '$stateParams', 'posts',
+	function($scope, $stateParams, posts) {
+		// body...
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function(){
+		  if($scope.body === '') { return; }
+		  $scope.post.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
 		};
 	}]);
