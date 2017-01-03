627836fef341223411f59f466868465812806383
Abati Adewale <acekyd01@gmail.com>
2017-01-04 00:11:44 +0100

    login and register endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 
 /* GET home page. */
@@ -70,5 +73,39 @@ router.post('/posts/:post/comments', function(req, res, next) {
     });
   });
 });
+
+router.post('/register', function(req, res, next){
+	if(!req.body.username || !req.body.password){
+		return res.status(400).json({message: 'Please fill out all fields'});
+	}
+
+	var user = new User();
+
+	user.username = req.body.username;
+
+	user.setPassword(req.body.password)
+
+	user.save(function (err){
+		if(err){ return next(err); }
+
+		return res.json({token: user.generateJWT()})
+	});
+});
+
+router.post('/login', function(req, res, next){
+	if(!req.body.username || !req.body.password){
+		return res.status(400).json({message: 'Please fill out all fields'});
+	}
+
+	passport.authenticate('local', function(err, user, info){
+		if(err){ return next(err); }
+
+		if(user){
+			return res.json({token: user.generateJWT()});
+		} else {
+			return res.status(401).json(info);
+		}
+	})(req, res, next);
+})
 
 module.exports = router;
