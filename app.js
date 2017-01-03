42d0b05c491e305bef3d7966d98826b26b9c9bfe
Abati Adewale <acekyd01@gmail.com>
2017-01-03 23:57:38 +0100

    passport configured

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', index);
 app.use('/users', users);
