b4c2133d1a9aac2eb1b4684bea2112e693a9718c
Abati Adewale <acekyd01@gmail.com>
2017-01-10 23:44:13 +0100

    add authors

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/posts', function(req, res, next) {
 
 router.post('/posts', auth, function(req, res, next){
 	var post = new Post(req.body);
-
+	post.author = req.payload.username;
 	post.save(function(err, post){
 		if(err){ return next(err); }
 
@@ -65,6 +65,7 @@ router.put('/posts/:post/upvote', auth, function(req, res, next) {
 router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment){
     if(err){ return next(err); }
