db8c6e4240cce7f77b7d632143f9bcbb205cf55c
Abati Adewale <acekyd01@gmail.com>
2017-01-14 16:15:19 +0100

    angular auth

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -37,6 +37,57 @@ app.factory('posts', ['$http', function($http) {
 	    });
 	};
 	return o;
+}])
+.factory('auth', ['$http', '$window', function($http, $window){
+	var auth = {};
+
+	auth.saveToken = function (token) {
+		$window.localStorage['flapper-news-token'] = token;
+	};
+
+	auth.getToken = function() {
+		return $window.localStorage['flapper-news-token'];
+	}
+
+	auth.isLoggedIn = function () {
+		var token = auth.getToken();
+
+		if(token) {
+			var payload = JSON.parse($window.atob(token.split(.)[1]));
+
+			return payload.exp > Date.now() / 1000;
+		}
+		else {
+			return false;
+		}
+	}
+
+	auth.currentUser = function(){
+		if(auth.isLoggedIn()){
+			var token = auth.getToken();
+			var payload = JSON.parse($window.atob(token.split(.)[1]));
+
+			return payload.username;
+		}
+	}
+
+	auth.register = function(user) {
+		return $http.post('/register', user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	}
+
+	auth.login = function(user){
+		return $http.post('/login', user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	}
+
+	auth.logout = function(){
+		$window.localStorage.removeItem('flapper-news-token');
+	}
+
+	return auth;
 }]);
 
 app.config([
